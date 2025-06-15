32fc80ac5e842f10ad32f7e51ee61aa02dd7bb2c
Chandrashekhar Sharma <sharmachiman6@gmail.com>
2025-06-15 11:53:06 +0530

    small changes are ramaining

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,9 @@ import Projects from "./pages/projects";
 import Contact from "./pages/contact";
 import Notfound from "./pages/404";
 
-import { TRACKING_ID } from "./data/tracking";
 import "./app.css";
 
 function App() {
-	useEffect(() => {
-		if (TRACKING_ID !== "") {
-			ReactGA.initialize(TRACKING_ID);
-		}
-	}, []);
 
 	return (
 		<div className="App">
